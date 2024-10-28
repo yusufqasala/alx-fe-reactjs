733122dfb88e4a8f5c5e3dace77d04cbb48aa7df
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-10-28 18:46:12 +0300

    Create UserProfile.jsx

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+export default function UserProfile(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+}
