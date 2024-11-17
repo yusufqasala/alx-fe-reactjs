d296cbeb94e5dce660732305172a01ab556a861b
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-11-17 19:43:18 +0200

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,15 +1,15 @@
-import { useRecipeStore } from "./recipeStore";
+import useRecipeStore from "../store/recipeStore";
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
 
   return (
     <div>
       <h2>Recipe List</h2>
-      {recipes.length === 0 ? (
-        <p>No recipes available. Add some!</p>
+      {filteredRecipes.length === 0 ? (
+        <p>No recipes found. Try adjusting your search or filters.</p>
       ) : (
-        recipes.map((recipe) => (
+        filteredRecipes.map((recipe) => (
           <div
             key={recipe.id}
             style={{
