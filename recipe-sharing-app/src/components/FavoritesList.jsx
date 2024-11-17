4fdf7ed117488d4e15d427bf0f0b284bf64b2d27
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-11-17 19:53:31 +0200

    Update FavoritesList.jsx

--- recipe-sharing-app/src/components/FavoritesList.jsx
+++ recipe-sharing-app/src/components/FavoritesList.jsx
@@ -1,4 +1,4 @@
-import { useRecipeStore } from "../store/recipeStore";
+import { useRecipeStore } from "./recipeStore";
 
 const FavoritesList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
