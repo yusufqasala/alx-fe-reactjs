b40d25e054001e55905e99301ea97a97ef6d86ba
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-11-19 18:08:09 +0200

    Update ProtectedRoute .jsx

--- react-router-advanced/src/components/ProtectedRoute .jsx
+++ react-router-advanced/src/components/ProtectedRoute .jsx
@@ -1,11 +1,13 @@
+import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ isAuthenticated, children }) => {
-  if (!isAuthenticated) {
-    return <Navigate to="/" />;
-  }
+    if (!isAuthenticated) {
+        // Redirect to home if not authenticated
+        return <Navigate to="/" />;
+    }
 
-  return children;
+    return children; // Render children components if authenticated
 };
 
 export default ProtectedRoute;
