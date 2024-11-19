05a42f95f3eaa58fa1f059c4d54ded1dc1c6cbb0
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-11-19 17:35:33 +0200

    Add files via upload

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import PostsComponent from './components/PostsComponent';
+
+const App = () => {
+    return (
+        <div>
+            <PostsComponent />
+        </div>
+    );
+};
+
+export default App;
---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,38 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { useQuery } from "react-query";
+
+const fetchPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+};
+
+const PostsComponent = () => {
+  const { data, error, isLoading, isError, refetch } = useQuery(
+    "posts",
+    fetchPosts
+  );
+
+  if (isLoading) return <p>Loading posts...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <button onClick={refetch}>Refetch Posts</button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
