b85cbc1d2ed7f9d3025f77af27c919405eaf696d
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-11-19 18:03:42 +0200

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,8 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BlogPost from "./components/BlogPost";
 import Profile from './components/Profile';
+import ProtectedRoute from './components/ProtectedRoute ';
+
 
 const App = () => {
+    const isAuthenticated = true;
+
   return (
     <BrowserRouter>
       <Routes>
@@ -11,6 +15,14 @@ const App = () => {
         <Route path="/profile/*" element={<Profile />} />
         {/* Dynamic Route for Blog Posts */}
         <Route path="/blog/:id" element={<BlogPost />} />
+      <Route
+        path="/profile/*"
+        element={
+          <ProtectedRoute isAuthenticated={isAuthenticated}>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
       </Routes>
     </BrowserRouter>
   );
