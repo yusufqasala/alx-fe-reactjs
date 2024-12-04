4d99749239f81456c7c672f9c5df8864a9eaa5bd
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-12-04 16:14:13 +0200

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,10 +1,15 @@
-import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
