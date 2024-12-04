220d34e29d36b6305f06173a364c8138b5c5e1c6
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-12-04 16:16:18 +0200

    Add files via upload

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import RecipeDetail from './components/RecipeDetail';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   return (
--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,19 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const HomePage = () => {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    fetch('/src/data.json')
+    fetch("/src/data.json")
       .then((response) => response.json())
       .then((data) => setRecipes(data))
-      .catch((error) => console.error('Error fetching recipes:', error));
+      .catch((error) => console.error("Error fetching recipes:", error));
   }, []);
 
   return (
     <div className="p-4">
-      <h1 className="text-3xl font-bold text-center mb-6">Recipe Sharing Platform</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <h1 className="text-3xl font-bold text-center mb-6">
+        Recipe Sharing Platform
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {recipes.map((recipe) => (
           <div
             key={recipe.id}
