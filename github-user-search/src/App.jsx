de81946487122555c22aac0230e22ad923ad50a3
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-12-15 15:34:17 +0200

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,8 +1,10 @@
+import SearchBar from './components/SearchBar';
+
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <p>Search for GitHub profiles using the search bar above.</p>
+      <SearchBar />
     </div>
   );
 }
