bff3fa948c620e8655e3015dd1b85293e37ac821
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-12-15 15:46:32 +0200

    Update Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -39,7 +39,7 @@ function Search() {
       </form>
 
       {loading && <p>Loading...</p>}
-      {error && <p>Looks like we can't find the user.</p>}
+      {error && <p>Looks like we cant find the user.</p>}
 
       {user && (
         <div>
