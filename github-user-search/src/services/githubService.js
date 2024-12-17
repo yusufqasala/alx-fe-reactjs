c2546edc8731be3cfe7d8cdddf8e79a8700b8f60
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-12-17 15:56:03 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -22,4 +22,41 @@ export const fetchAdvancedUserData = async ({ username = '', location = '', minR
     console.error('Error fetching advanced user data:', error.message);
     throw error;
   }
+
+   let query = `${searchTerm}`;
+    if (location) {
+      query += `+location:${location}`;
+    }
+    if (minRepos) {
+      query += `+repos:>${minRepos}`;
+    }
+
+    // Make the GET request to the GitHub Search API
+    const response = await axios.get(`${BASE_URL}?q=${query}`);
+
+    // Return the user data
+    return response.data.items; // 'items' contains the list of users
+  } catch (error) {
+    console.error('Error fetching GitHub users:', error.message);
+    throw error;
+  }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
