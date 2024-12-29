745b19a06e67bbfe0415b0bae45fa6e4c623f33a
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-12-29 18:53:52 +0200

    Add files via upload

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+      }}
+    >
+      <p>&copy; 2024 My Favorite Cities</p>
+    </footer>
+  );
+};
+
+export default Footer;
