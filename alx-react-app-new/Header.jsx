c3e09dabe95f33b4346d8c209d71ee0eb7dbb12f
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-12-29 18:50:54 +0200

    Add files via upload

---
+++ alx-react-app-new/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header
+      style={{
+        backgroundColor: "navy",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+      }}
+    >
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ alx-react-app-new/MainContent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <main
+      style={{ padding: "20px", backgroundColor: "#f4f4f4", minHeight: "60vh" }}
+    >
+      <p>
+        Welcome to the main content section! Here you can explore various cities
+        and learn more about them.
+      </p>
+    </main>
+  );
+};
+
+export default MainContent;
