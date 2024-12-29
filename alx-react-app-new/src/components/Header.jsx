33af08269bb3449165697f68ba1780d5390e44ac
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-12-29 18:55:44 +0200

    Update Header.jsx

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,15 +1,9 @@
-import React from "react";
+// Header.jsx
+import React from 'react';
 
 const Header = () => {
   return (
-    <header
-      style={{
-        backgroundColor: "navy",
-        color: "white",
-        textAlign: "center",
-        padding: "10px",
-      }}
-    >
+    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px' }}>
       <h1>My Favorite Cities</h1>
     </header>
   );
