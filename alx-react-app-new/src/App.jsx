2293d6d509e71c25057f393652047ad5e2e5d46d
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-12-29 19:01:52 +0200

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,20 +1,15 @@
 // src/App.jsx
 import React from 'react';
-import Header from './components/Header';
-import UserProfile from './components/UserProfile';
-import Footer from './components/Footer';
-import Counter from './components/Counter';
+import Counter from './components/Counter.jsx'; // Make sure this path is correct based on your folder structure
 
 function App() {
   return (
     <div>
-      <Header />
-      <UserProfile />
       <h1>Counter Application</h1>
       <Counter />
-      <Footer />
     </div>
   );
 }
 
 export default App;
+
