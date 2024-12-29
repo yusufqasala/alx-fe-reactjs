4cd04e4ab124f6e8df71a4d3e80c66bd00c27038
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2024-12-29 18:56:07 +0200

    Update and rename MainContent.jsx to UserProfile.jsx

---
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,14 @@
+// UserProfile.jsx
+import React from 'react';
+
+const UserProfile = (props) => {
+  return (
+    <div style={{ border: '1px solid gray', padding: '20px', margin: '10px', borderRadius: '5px' }}>
+      <h2 style={{ color: 'blue', fontSize: '24px' }}>{props.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold', color: 'green' }}>{props.age}</span></p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
