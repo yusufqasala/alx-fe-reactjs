9e44b40da63d44bb8ba78cfa9ce1fa43eb57e1cf
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2025-01-19 15:29:53 +0200

    Add files via upload

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', backgroundColor: '#ddd', textAlign: 'center' }}>
+        <p>&copy; 2025 My Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
